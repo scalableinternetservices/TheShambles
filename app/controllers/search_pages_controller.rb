582e2424b5b83209b2531be4e574112c6d911cd7
Nazmus Saquib <saquib2527@yahoo.com>
2018-11-16 22:25:52 -0800

    update search games based on number of backgrounds so that it works

--- app/controllers/search_pages_controller.rb
+++ app/controllers/search_pages_controller.rb
@@ -24,11 +24,11 @@ class SearchPagesController < ApplicationController
 	  render "search_result_game_by_company"
 	end
       elsif params[:search_type] == "game_by_background_count"
-		params[:minimum_Background] = params[:minimum_Background].empty? ? 1 : params[:minimum_Background]
-		params[:maximum_Background] = params[:maximum_Background].empty? ? 10 : params[:maximum_Background]
-		@games = Game.select("games.*").joins(:backgrounds).group("backgrounds.game_id").having("count(backgrounds.game_id) >= :min AND count(backgrounds.game_id) <= :max", {min: params[:minimum_Background].to_i, max: params[:maximum_Background].to_i})
-		@search_title = "Games with #{params[:minimum_Background]} to #{params[:maximum_Background]} number of background"
-		render "search_result_game_by_price_range"
+	params[:minimum_Background] = params[:minimum_Background].empty? ? 1 : params[:minimum_Background]
+	params[:maximum_Background] = params[:maximum_Background].empty? ? 10 : params[:maximum_Background]
+	@games = Game.joins(:backgrounds).group('games.id').having("count(games.id) >= #{params[:minimum_Background]} and count(games.id) <= #{params[:maximum_Background]}")
+	@search_title = "Games with #{params[:minimum_Background]} to #{params[:maximum_Background]} number of background"
+	render "search_result_game_by_price_range"
       elsif params[:search_type] == "game_by_card_range"
         params[:minimum_number_of_cards] = params[:minimum_number_of_cards].empty? ? 0 : params[:minimum_number_of_cards]
         params[:maximum_number_of_cards] = params[:maximum_number_of_cards].empty? ? 15 : params[:maximum_number_of_cards]
