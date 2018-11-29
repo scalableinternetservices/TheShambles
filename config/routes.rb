3794aa8b060cef61c56045c53a4b6d1fa648bda4
Nazmus Saquib <saquib2527@yahoo.com>
2018-11-29 11:48:13 -0800

    add route for ajax

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,6 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   get '/chat', to: 'chatrooms#show'
   resources :messages, only: [:create]
+  get '/games/stats/:id', to: 'games#stats'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
