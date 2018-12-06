711f7da7da847dd4a723d63389298924c876b48f
magzhanz <zmagzhanz@gmail.com>
2018-12-06 16:45:03 +0000

    boom

--- app/controllers/search_pages_controller.rb
+++ app/controllers/search_pages_controller.rb
@@ -83,7 +83,7 @@ INNER JOIN system_requirements
 ON games.id = system_requirements.game_id
 INNER JOIN graphics
 ON system_requirements.graphic_id = graphics.id
-WHERE graphics.rank >= #{graphic_rank}
+WHERE graphics.rank >= #{graphic_rank} limit 100
       """
       @games = ActiveRecord::Base.connection.execute(sql)
       @search_title = "Games You can Run"
